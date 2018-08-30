f5e3e7c493dc2071d5ec307eb1fe3b580b176f8f
ldaveyl <lucas.davey1@gmail.com>
2018-08-30 14:50:46 +0200

    added biopython license

--- Bio/SearchIO/AnarciIO/anarci_num.py
+++ Bio/SearchIO/AnarciIO/anarci_num.py
@@ -1,4 +1,7 @@
 # Copyright 2018 by Lucas Davey.  All rights reserved.
+# This code is part of the Biopython distribution and governed by its
+# license.  Please see the LICENSE file that should have been included
+# as part of this package.
 
 """Bio.SearchIO parser for Anarci numbered output formats."""
 
